0436a2454e3cba20911d09eb429c920eaa27839a
devegoo <57900186+devegoo@users.noreply.github.com>
2021-06-20 22:59:58 +0200

    Add files via upload

---
+++ build_kernel/autobuild.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+H=$(date +%H)
+if (( 0 <= $H && $H < 4 )); then
+	echo between 0AM and 3AM
+        cd /home/kernels/linux-tkg/
+	echo 'curl -s https://www.kernel.org | grep -A1 latest_link | tail -n1 | egrep -o '>[^<]+' | egrep -o '[^>]+'' > kernel_version
+	echo 'cat kernel_version'
+	./start_kernel_make.sh
+elif (( 3 <= $H && $H < 23 )); then
+	echo between 3AM and 0PM nothing to do now
+else
+	echo nothing to do
+fi
