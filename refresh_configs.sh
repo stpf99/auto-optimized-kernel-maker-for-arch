bfff8abd63019fa532cc907b7e92c69fe38b92d6
devegoo <57900186+devegoo@users.noreply.github.com>
2021-06-21 21:16:32 +0200

    Update refresh_configs.sh

--- refresh_configs.sh
+++ refresh_configs.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-sudo  rm -r /home/kernels/linux-tkg
-cd /home/kernels/
+sudo  rm -r /mnt/sdb1/kernels/linux-tkg
+cd /mnt/sdb1/kernels/
 pacman -Syu git
 git clone https://github.com/Frogging-Family/linux-tkg.git
-echo "Edit default Options for your individual kernel customization in /home/kernels/linux-tkg/customization.cfg file. Answer to any Question and save."
-echo set autostart for your computer in Bios at "1 AM everyday"
-echo "After this two steps Your computer will start on 1AM and kernel autobuild starts if hour is between 1AM - 3AM and if new rc kernel release is available.
+echo "
+
+Edit default Options for your individual kernel customization in /home/kernels/linux-tkg/customization.cfg file. Answer to any Question and save."
+echo "
+
+Set autostart for your computer in Bios at 1 AM everyday"
+echo "
+
+After this two steps Your computer will start on 1AM and kernel autobuild starts if hour is between 1AM - 3AM and if new rc kernel release is available"
