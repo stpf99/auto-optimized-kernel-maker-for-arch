647b52cc779dcbe9acaa73767dd7dd8a3fd59f31
devegoo <57900186+devegoo@users.noreply.github.com>
2021-06-21 21:30:59 +0200

    Update start_kernel_make.sh

--- start_kernel_make.sh
+++ start_kernel_make.sh
@@ -5,9 +5,9 @@ cd /mnt/sdb1/kernels/linux-tkg/
 mv customization.cfg backup_customization.cfg
 echo 'grep -vwE "*.**"' backup_customization.cfg > kernel_version_local
 if $VER > $LOCAL_VER then
-grep -vwE "_version="*.**" backup_customization.cfg > customization.cfg
-echo _version="$VER" > customization.cfg
+grep -vwE '_version="*.**' backup_customization.cfg > customization.cfg
+echo '_version=$VER' > customization.cfg
 makepkg -s
 else 
-echo Kernel version is as Local version /not compiling
+echo "not new kernel found /not compiling"
 fi
