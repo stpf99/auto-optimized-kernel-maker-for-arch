f27ea2cdc8eabb756953eb71bfc522270f1406f9
devegoo <57900186+devegoo@users.noreply.github.com>
2021-06-21 20:59:55 +0200

    Add files via upload

--- before_first_run.sh
+++ before_first_run.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-dd if=/dev/zero of=/mnt/sdb1/swapfile bs=1M count=8192
+#sudo dd if=/dev/zero of=/mnt/sdb1/swapfile bs=1M count=8192
 cd /mnt/sdb1/
-mkswap /swapfile
-swapon swapfile
-sudo pacman -Syu lld clang llvm bison xmlto docbook-xsl kmod inetutils bc libelf pahole patchutils flex python-sphinx python-sphinx_rtd_theme graphviz imagemagick git cpio perl tar xz schedtool strip docs
-cd ~/
+sudo mkswap swapfile
+sudo swapon swapfile
+#sudo pacman -Syu lld clang llvm bison xmlto docbook-xsl kmod inetutils bc libelf pahole patchutils flex python-sphinx python-sphinx_rtd_theme graphviz imagemagick git cpio perl tar xz schedtool
 git clone https://aur.archlinux.org/modprobed-db.git
-cd modpp	robed-db
-makepkg si
+cd modprobed-db
+makepkg -si
 touch ~/.config/modprobed.db
 modprobed-db
 modprobed-db list
