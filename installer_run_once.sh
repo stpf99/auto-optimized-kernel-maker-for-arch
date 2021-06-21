17904f3d6f1b08a725496ed55e0b25abc55c900c
devegoo <57900186+devegoo@users.noreply.github.com>
2021-06-21 21:13:46 +0200

    Add files via upload

--- installer_run_once.sh
+++ installer_run_once.sh
@@ -3,11 +3,19 @@ sudo cp systemd/kernel_autobuild.service /etc/systemd/system
 sudo systemctl daemon-reload
 sudo systemctl enable kernel_autobuild.service
 sudo systemctl start kernel_autobuild.service
-mkdir /home/kernels
-cp start_kernel_make.sh refresh_configs.sh build_kernel/autobuild.sh /home/kernels/
-cd /home/kernels/
-pacman -Syu git
+mkdir /mnt/sdb1/kernels
+cp start_kernel_make.sh refresh_configs.sh build_kernel/autobuild.sh /mnt/sdb1/kernels/
+cd /mnt/sdb1/kernels/
+sudo pacman -Syu git
 git clone https://github.com/Frogging-Family/linux-tkg.git
-echo "Edit default Options for your individual kernel customization in /home/kernels/linux-tkg/customization.cfg file. Answer to any Question and save."
-echo set autostart for your computer in Bios at "1 AM everyday"
-echo "After this two steps Your computer will start on 1AM and kernel autobuild starts if hour is between 1AM - 3AM and if new rc kernel release is available.
+echo "
+
+Edit default Options for your individual kernel customization in /mnt/sdb1/kernels/linux-tkg/customization.cfg file. Answer to any Question and save."
+
+echo "
+
+Set autostart for your computer in Bios at 1 AM everyday"
+
+echo "
+
+After this two steps Your computer will start on 1AM and kernel autobuild starts if hour is between 1AM - 3AM and if new rc kernel release is available"
