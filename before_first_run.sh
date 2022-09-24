7f98248effda19f00e795b6bda4c8fba5cdf9f54
Tomasz Świątek <57900186+devegoo@users.noreply.github.com>
2022-09-24 17:32:34 +0200

    Update before_first_run.sh

--- before_first_run.sh
+++ before_first_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#sudo dd if=/dev/zero of=/mnt/sdb1/swapfile bs=1M count=8192
-cd /mnt/sdb1/
+#sudo dd if=/dev/zero of=/home/$USER/swapfile bs=1M count=8192
+cd /home/$USER
 sudo mkswap swapfile
 sudo swapon swapfile
 #sudo pacman -Syu lld clang llvm bison xmlto docbook-xsl kmod inetutils bc libelf pahole patchutils flex python-sphinx python-sphinx_rtd_theme graphviz imagemagick git cpio perl tar xz schedtool
